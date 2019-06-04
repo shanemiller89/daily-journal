bc226f60e2e981c97aed01d89c3abbd672e6dfdb
Shane Miller <shanemiller89@gmail.com>
2019-06-04 12:43:30 -0500

    Added object data structure, added array, created push function, all for collection of journals

---
+++ src/scripts/journal.js
@@ -0,0 +1,13 @@
+let collectionJournalEntry = []
+
+
+const objectsJournalEntry = {
+    entry_date: "" ,
+    entry_title: "" ,
+    entry_text: "" ,
+    entry_mood: ""
+}
+
+function addJournal(){
+    collectionJournalEntry.push(objectsJournalEntry)
+}
