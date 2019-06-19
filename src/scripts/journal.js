ef2548998c4d7c79020aea2532d2fc013ca1fdcc
Shane Miller <shanemiller89@gmail.com>
2019-06-19 12:00:34 -0500

    Added validation function an post to API function

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -8,13 +8,22 @@ API.getJournalEntries().then(entries_obj => {
 
 const submitBtn = document.querySelector(".submit-btn");
 
-// submitBtn.addEventListener("click", )
+submitBtn.addEventListener("click", () => {
+  formValidation();
+  if (formValidation === true) {
+    let newJournalEntry = createNewEntry(journalDateInput, conceptsCoveredInput, journalEntryInput, moodSelectInput);
+    postJournalEntry(newJournalEntry);
+  } else {
+    alert ("Please finish filling out your Journal!")
+  }
+})
 
 // -- Input Fields -- //
 let journalDateInput = document.querySelector("#journalDate");
 let conceptsCoveredInput = document.querySelector("#conceptsCovered");
 let journalEntryInput = document.querySelector("#journalEntry");
 let moodSelectInput = document.querySelector("#moodSelect");
+
 
 // -- Factory function for journal entries -- //
 
@@ -24,7 +33,42 @@ function createNewEntry(date, concept, entry, mood) {
     "entry_title": concept.value,
     "entry_text": entry.value,
     "entry_mood": mood.value
-  }
+  };
 }
 
-console.log(createNewEntry(journalDateInput, conceptsCoveredInput, journalEntryInput, moodSelectInput))
+// -- Form Validation -- //
+function formValidation() {
+  if(journalDateInput.value == "") {
+    alert("Please provide a date!");
+    journalDateInput.focus() ;
+    return false;
+  }
+  if(conceptsCoveredInput.value == "") {
+    alert("Please provide what was covered!");
+    conceptsCoveredInput.focus() ;
+    return false;
+  }
+  if(journalEntryInput.value == "") {
+    alert("Please provide a 'Journal Entry!'");
+    journalEntryInput.focus() ;
+    return false;
+  }
+  if(moodSelectInput.value == "") {
+    alert("Please provide your mood!");
+    moodSelectInput.focus() ;
+    return false;
+  }
+  return( true );
+}
+
+// -- Post Data to Database -- //
+
+function postJournalEntry(entry) {
+  fetch("http://localhost:8088/collectionJournalEntries", { 
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(entry)
+})
+}
