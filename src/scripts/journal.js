a207a2333a88d8259c0124be6eb29c6a5ee1fa99
Shane Miller <shanemiller89@gmail.com>
2019-06-18 22:06:59 -0500

    factory function and start of event listener

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -3,3 +3,28 @@
 API.getJournalEntries().then(entries_obj => {
   RENDER.insertComponent(entries_obj);
 });
+
+// -- Submit Button Listener -- //
+
+const submitBtn = document.querySelector(".submit-btn");
+
+// submitBtn.addEventListener("click", )
+
+// -- Input Fields -- //
+let journalDateInput = document.querySelector("#journalDate");
+let conceptsCoveredInput = document.querySelector("#conceptsCovered");
+let journalEntryInput = document.querySelector("#journalEntry");
+let moodSelectInput = document.querySelector("#moodSelect");
+
+// -- Factory function for journal entries -- //
+
+function createNewEntry(date, concept, entry, mood) {
+  return {
+    "entry_date": date.value,
+    "entry_title": concept.value,
+    "entry_text": entry.value,
+    "entry_mood": mood.value
+  }
+}
+
+console.log(createNewEntry(journalDateInput, conceptsCoveredInput, journalEntryInput, moodSelectInput))
